9ba210bed5f7445f2ffe8a3b1acc438fc6007d6a
Kush Patel <kushpatel35@hotmail.com>
2016-05-08 13:53:06 -0400

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(1042, 531)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("https://wrem.sis.yorku.ca/Apps/WebObjects/REM ", System.UriKind.Absolute)
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 21)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(249, 162)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(83, 126)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(72, 88)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.Text = "Course Code"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(72, 52)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "Password"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(72, 16)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "User"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1042, 531)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "York Enrolment Bot | Kush Patel"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,176 @@
+﻿Public Class Form1
+
+    Dim user As String = ""
+    Dim pass As String = ""
+
+    Dim courseCode As String = ""
+    Dim codes As ArrayList = Nothing
+    Dim currentDex As Integer = -1
+
+    Dim rnd As New Random
+    Dim loopsLeft As Integer = rnd.Next(20, 26)
+
+    Private Sub fillLogIn()
+        Dim userBox As HtmlElement = Nothing
+        Dim passBox As HtmlElement = Nothing
+        Dim loginButton As HtmlElement = Nothing
+
+        For Each h As HtmlElement In WebBrowser1.Document.All
+            If h <> Nothing And h.Name <> Nothing Then
+                If h.Name.Equals("mli") Then
+                    userBox = h
+                ElseIf h.Name.Equals("password") Then
+                    passBox = h
+                ElseIf h.Name.Equals("dologin") Then
+                    loginButton = h
+                End If
+            End If
+        Next
+
+        If userBox <> Nothing And passBox <> Nothing And loginButton <> Nothing Then
+            userBox.InnerText = user
+            passBox.InnerText = pass
+            loginButton.InvokeMember("click")
+        End If
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        Dim url As String = e.Url.ToString
+        If url.Equals("https://passportyork.yorku.ca/ppylogin/ppylogin") And user <> Nothing Then
+            fillLogIn()
+        ElseIf url.Equals("https://wrem.sis.yorku.ca/Apps/WebObjects/REM.woa/wa/DirectAction/rem") Then
+            Dim selectEle As HtmlElement = Nothing
+            Dim continueEle As HtmlElement = Nothing
+            For Each h As HtmlElement In WebBrowser1.Document.All
+                If h <> Nothing And h.Name <> Nothing Then
+                    If h.Name.Equals("3.5.1.27.1.11") Then
+                        selectEle = h
+                    ElseIf h.Name.Equals("3.5.1.27.1.13") Then
+                        continueEle = h
+                    End If
+                End If
+            Next
+            If selectEle <> Nothing And continueEle <> Nothing Then
+                selectEle.SetAttribute("value", "1")
+                continueEle.InvokeMember("click")
+            End If
+        ElseIf url.Contains("/wo/") Then
+            If WebBrowser1.DocumentTitle.Equals("REM - Summary") Then
+                Threading.Thread.Sleep(rnd.Next(5000, 8001))
+                Dim addEle As HtmlElement = Nothing
+                For Each h As HtmlElement In WebBrowser1.Document.All
+                    If h <> Nothing And h.Name <> Nothing Then
+                        If h.Name.Equals("3.1.27.1.17") Then
+                            addEle = h
+                        End If
+                    End If
+                Next
+                If addEle <> Nothing Then
+                    addEle.InvokeMember("click")
+                End If
+            ElseIf WebBrowser1.DocumentText.Contains("Please key in the 6 digit catalogue number") Then
+                Dim courseBox As HtmlElement = Nothing
+                Dim addButton As HtmlElement = Nothing
+                For Each h As HtmlElement In WebBrowser1.Document.All
+                    If h <> Nothing And h.Name <> Nothing Then
+                        If h.Name.Equals("3.1.27.5.7") Then
+                            courseBox = h
+                        ElseIf h.Name.Equals("3.1.27.5.9") Then
+                            addButton = h
+                        End If
+                    End If
+                Next
+                If courseBox <> Nothing And addButton <> Nothing Then
+                    If currentDex <> -1 Then
+                        courseCode = codes.Item(currentDex)
+                        If currentDex < (codes.Count - 1) Then
+                            currentDex += 1
+                        Else
+                            currentDex = 0
+                        End If
+                    End If
+                    courseBox.InnerText = courseCode
+                    addButton.InvokeMember("click")
+                End If
+            ElseIf WebBrowser1.DocumentText.Contains("Please confirm that you want to:") Then
+                Dim yesButton As HtmlElement = Nothing
+                For Each h As HtmlElement In WebBrowser1.Document.All
+                    If h <> Nothing And h.Name <> Nothing Then
+                        If h.Name.Equals("3.1.27.7.9") Then
+                            yesButton = h
+                        End If
+                    End If
+                Next
+                If yesButton <> Nothing Then
+                    yesButton.InvokeMember("click")
+                End If
+            ElseIf WebBrowser1.DocumentText.Contains("Result:") Then
+                If WebBrowser1.DocumentText.Contains("The course has not been added.") Then
+                    Dim continueEle As HtmlElement = Nothing
+                    For Each h As HtmlElement In WebBrowser1.Document.All
+                        If h <> Nothing And h.Name <> Nothing Then
+                            If h.Name.Equals("3.1.27.15.9") Then
+                                continueEle = h
+                            End If
+                        End If
+                    Next
+                    If continueEle <> Nothing Then
+                        loopsLeft -= 1
+                        If loopsLeft = 0 Then
+                            Dim logoutEle As HtmlElement = Nothing
+                            For Each h As HtmlElement In WebBrowser1.Document.All
+                                If h <> Nothing And h.Name <> Nothing Then
+                                    If h.Name.Equals("3.1.3.1.1") Then
+                                        logoutEle = h
+                                    End If
+                                End If
+                            Next
+                            If logoutEle <> Nothing Then
+                                logoutEle.InvokeMember("click")
+                            End If
+                            loopsLeft = rnd.Next(3, 5)
+                        Else
+                            continueEle.InvokeMember("click")
+                        End If
+                    Else
+                        Me.Text = Me.Text + " | " + courseCode
+                        If currentDex <> -1 Then
+                            codes.Remove(courseCode)
+                            If codes.Count = 0 Then
+                                MsgBox("Operation Complete")
+                            End If
+                        Else
+                            MsgBox("Operation Complete")
+                        End If
+                    End If
+                End If
+            End If
+        ElseIf url.Equals("https://passportyork.yorku.ca/ppylogin/ppylogout") Then
+            WebBrowser1.Navigate("http://google.com")
+        ElseIf url.Equals("http://www.google.ca/") Then
+            Threading.Thread.Sleep(rnd.Next(60000, 120001))
+            WebBrowser1.Navigate("https://wrem.sis.yorku.ca/Apps/WebObjects/REM")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        user = TextBox1.Text
+        pass = TextBox2.Text
+        courseCode = TextBox3.Text
+        If courseCode.Contains(",") Then
+            codes = New ArrayList(courseCode.Split(","))
+            currentDex = 0
+        End If
+        Panel1.Dispose()
+        fillLogIn()
+    End Sub
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
